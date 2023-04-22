55d05d35ede1699b4ec994f628b5a320f74ccf1f
gustav018 <gustavofleitas18@gmail.com>
2023-04-22 15:55:25 -0400

    actualizacion js

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ CALCULAR.addEventListener('click', () => {
         MAN.style.display = 'none';
     }
 })
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;
+    if (resto>20){
+        let aux = resto-20;
+        flujo += aux*1;
+        resto -= aux;
+    } 
+    if (resto>10){
+        let aux = resto-10;
+        flujo += aux*2;
+        resto -= aux;
+    }
+    flujo += resto*4;
+    return flujo;
+}
